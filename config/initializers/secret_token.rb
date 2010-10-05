12e742b2b3130f306bd87af48cfd05a64b84dab4
nakaji <nakaji4923@gmail.com>
2010-10-05 16:44:50 +0900

    generate scaffold post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '52d23a554863552168aeacf600ed8138c90d1a9e99de95d3a3bc954340e4a50adafb9d15adf30de1e43832cfa7caab6d7366481577cd40d3a881597ee8dd7aba'
